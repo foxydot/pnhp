e4f6e28a86a38f50aa87c0391dca99e942ab1480
CMO <catherine@msdlab.com>
2018-05-24 16:54:09 -0400

    newsletter push

--- system/assets/themes/grandrounds/taxonomy-member_resources_category.php
+++ system/assets/themes/grandrounds/taxonomy-member_resources_category.php
@@ -3,7 +3,8 @@ remove_all_actions('msdlab_title_area' );
 remove_all_actions('genesis_entry_header');
 remove_all_actions('genesis_entry_content');
 remove_all_actions('genesis_entry_footer');
-add_action('genesis_before_loop','msdlab_mr_challenge');
+add_action('genesis_loop','msdlab_mr_info',8);
+add_action('genesis_loop','msdlab_mr_challenge',9);
 add_action('msdlab_title_area','msdlab_news_cleanup');
 add_action('msdlab_title_area','msdlab_mr_category_banner');
 function msdlab_mr_category_banner(){
@@ -31,29 +32,43 @@ function msdlab_mr_page_title($title){
 function msdlab_mr_title_unlink(){
     return false;
 }
+function msdlab_mr_info(){
+    $term = get_queried_object();
+    $headline = apply_filters('the_title',get_term_meta( $term->term_id, 'headline', true ));
+    $intro_text = apply_filters('the_content',get_term_meta( $term->term_id, 'intro_text', true ));
+    if(strlen($headline) > 0){
+        $ret[] = '<h3 class="archive-headline">'.$headline.'</h3>';
+    }
+    if(strlen($intro_text) > 0){
+        $ret[] = '<div class="archive-intro-text">'.$intro_text.'</div>';
+    }
+    print implode("/n",$ret);
+}
 function msdlab_mr_challenge(){
     global $post,$wpalchemy_media_access,$member_resource_info;
     global $login_message;
     $obj = get_queried_object();
     $cat_slug = $obj->slug;
 
-    switch($cat_slug){ // first check for cookie
-        case "newsletter":;
+    switch($cat_slug) { // first check for cookie
+        case "newsletter":
+            ;
         case "slideshow":
         case "webinar":
             //check for cookie
-            ts_data($_COOKIE);
-            if(!isset($_COOKIE['member_login']) || $_COOKIE['member_login'] != 'member'){
+            if (!isset($_COOKIE['member_login']) || $_COOKIE['member_login'] != 'member') {
                 //if cookie not exist, display input and die
-                if($login_message){print '<div classs="alert">'.$login_message.'</div>';}
+                if ($login_message) {
+                    print '<div classs="alert">' . $login_message . '</div>';
+                }
                 msdlab_mr_login_form();
                 return false;
             }
             //else, add the content to the entries
-            add_action('genesis_entry_content','msdlab_mr_content');
-        break;
+            add_action('genesis_entry_content', 'msdlab_mr_content');
+            break;
         default:
-            add_action('genesis_entry_content','msdlab_mr_content');
+            add_action('genesis_entry_content', 'msdlab_mr_content');
             break;
     }
 }
@@ -61,12 +76,35 @@ function msdlab_mr_content(){
     global $post,$wpalchemy_media_access,$member_resource_info;
     $obj = get_queried_object();
     $cat_slug = $obj->slug;
-    switch($cat_slug){
+    //get all the stuff
+    $ctr = 0;
+    $member_resource_info->the_meta();
+    while ($member_resource_info->have_fields('memberresource')) {
+        $mr[$ctr]['title'] = $member_resource_info->get_the_value('mr_title') != '' ? $member_resource_info->get_the_value('mr_title') : get_the_title();
+        $mr[$ctr]['file'] = $member_resource_info->get_the_value('mr_file') != '' ? $member_resource_info->get_the_value('mr_file') : false;
+        $mr[$ctr]['author'] = $member_resource_info->get_the_value('mr_author') != '' ? $member_resource_info->get_the_value('mr_author') : false;
+        $mr[$ctr]['date'] = $member_resource_info->get_the_value('mr_date') != '' ? $member_resource_info->get_the_value('mr_date') : get_the_date();
+        $mr[$ctr]['tease'] = $member_resource_info->get_the_value('mr_tease') != '' ? apply_filters('the_content',$member_resource_info->get_the_value('mr_tease')) : false;
+        $ctr++;
+    }
+    switch($cat_slug) {
         case "newsletter":
-            $member_resource_info->the_meta();
-            while($member_resource_info->have_fields('memberresource'))
-            $title = $member_resource_info->get_the_value('mr_title')!=''?$member_resource_info->get_the_value('mr_title'):get_the_title();
-            print $title;
+            foreach($mr AS $ctr => $r){
+                if($r['file']){
+                    print '<h3 class="member-resource-title"><a href="'.$r['file'].'">'.$r['title'].'</a></h3>';
+                } else {
+                    print '<h3 class="member-resource-title">'.$r['title'].'</h3>';
+                }
+                if($r['tease']){
+                    print '<div>';
+                    print '<a class="collapse-btn" data-toggle="collapse" href="#collapse-'.$post->ID.'-'.$ctr.'" role="button" aria-expanded="false" aria-controls="collapse-'.$post->ID.'-'.$ctr.'">In this issue <i class="fa fa-angle-down"><span class="screen-reader-text">expand</span></i></a>';
+                    print '<div class="member-resource-teaser collapse" id="collapse-'.$post->ID.'-'.$ctr.'">'.$r['tease'].'</div>';
+                    print '</div>';
+                }
+                if($r['file']){
+                    print '<a class="btn btn-primary" href="'.$r['file'].'">Download PDF <i class="fa fa-file-pdf-o"></i></a>';
+                }
+            }
             break;
         case "slideshow":
             break;
